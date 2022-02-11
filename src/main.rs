b0032bec03e38be41d0c9eac3351dedeb88b3178
董家宏 <dongjiahong@hotmail.com>
2022-02-11 18:06:57 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use slint::Model;
+use rand::seq::SliceRandom;
 slint::include_modules!();
 
 fn main() {
-    AppWindow::new().run();
+    let main_window = AppWindow::new();
+
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+    tiles.extend(tiles.clone());
+
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    let tiles_
 }
