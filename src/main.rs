1ac08f74bbc9e83a73875b699f3e9a0d0968edbf
董家宏 <dongjiahong@hotmail.com>
2022-02-12 11:47:38 +0800

    can run

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use slint::Model;
 use rand::seq::SliceRandom;
+use slint::{Model, Timer, VecModel};
+use std::rc::Rc;
+use std::time::Duration;
 slint::include_modules!();
 
 fn main() {
@@ -11,5 +13,39 @@ fn main() {
     let mut rng = rand::thread_rng();
     tiles.shuffle(&mut rng);
 
-    let tiles_
+    let tiles_model = Rc::new(VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.clone().into());
+
+    let main_window_weak = main_window.as_weak();
+    main_window.on_check_if_pair_solved(move || {
+        let tiles_model = tiles_model.clone();
+        let mut flipped_tiles = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.image_visible && !tile.solved);
+
+        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) =
+            (flipped_tiles.next(), flipped_tiles.next())
+        {
+            let is_pair_solved = t1 == t2;
+            if is_pair_solved {
+                t1.solved = true;
+                tiles_model.set_row_data(t1_idx, t1);
+                t2.solved = true;
+                tiles_model.set_row_data(t2_idx, t2);
+            } else {
+                let main_window = main_window_weak.unwrap();
+                main_window.set_disable_tiles(true);
+                Timer::single_shot(Duration::from_secs(1), move || {
+                    main_window.set_disable_tiles(false);
+                    t1.image_visible = false;
+                    tiles_model.set_row_data(t1_idx, t1);
+                    t2.image_visible = false;
+                    tiles_model.set_row_data(t2_idx, t2);
+                });
+            }
+        }
+    });
+
+    main_window.run();
 }
